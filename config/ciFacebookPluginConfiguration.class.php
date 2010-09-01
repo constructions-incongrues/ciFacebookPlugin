22d7921fadca1e1a8c883e7ce5ddcd1e09f4c7a1
Tristan RIVOALLAN <tristan@cafe.com>
2010-09-01 17:51:18 +0200

    [standards] Applied Symfony coding standards.

---
+++ config/ciFacebookPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ciFacebookPlugin configuration.
+ *
+ * @package     ciFacebookPlugin
+ * @subpackage  config
+ */
+class ciFacebookPluginConfiguration extends sfPluginConfiguration
+{
+    const VERSION = '1.0.0-DEV';
+
+    /**
+     * @see sfPluginConfiguration
+     */
+    public function initialize()
+    {
+    }
+}
